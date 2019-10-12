a33ade017672cecd389f93d66fdd4716e4e12c18
norberto80@gmail.com <namatiz80@gmail.com>
2019-10-11 19:38:30 -0500

    Tareas React V1.6

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ class App extends Component {
    this.handleChange = this.handleChange.bind(this);
    this.handleKeyPress = this.handleKeyPress.bind(this);
  }
-
+//funcion para detectar el cambio de estado de 'task', el evento (e), controla el momento que cambia
  handleChange(e) {
    this.setState({
      task: e.target.value
    });
  }
-
+//funcion para almacenar la nueva propiedad, una ves se detecta el enter en el evento:   "e.key === 'Enter'", se almacena con el concatpara ir almacenando los estados de alltasks
  handleKeyPress(e) {
    if (e.key === 'Enter') {
      this.setState({
@@ -32,11 +32,16 @@ class App extends Component {
        <div className="list">
          <h3>Por hacer:</h3>
          <ul className="todo">
-           {this.state.alltasks.map(alltask =>
-             <li key={alltask}>{alltask}</li>
+{/*Se puede manejar con indice o sin indice todos los elementos del constructor.   Con el map se recorre el arreglo */}
+{/*la etiqueta <li> renderiza el map del elemento*/}
+        {/*{this.state.alltasks.map(alltask => <li key={alltask}>{alltask}</li>*/}
+           {this.state.alltasks.map((alltask, index) => <li key={index}>{alltask}</li>
            )}
          </ul>
          <div>
+{/*Con el onChange detectamos si cambia de estado el elemento. */}
+{/*Con el onKeyPress detectamos si hubo un evento como 'enter' en este caso, y onClick si hubo un 'click' en el campo de entrada*/}
+{/*Con el value controlamos el estado actual del elemento*/}
            <input type="text" id="new-task" placeholder="Ingresa una tarea y oprime Enter" onChange={this.handleChange} onKeyPress={this.handleKeyPress} value={this.state.task} />
          </div>
        </div>
